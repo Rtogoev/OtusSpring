31969cc015ee33585592be77c7bc74e392688bb1
rtogoev <rtogoev@megalabs.ru>
2020-03-20 15:26:47 +0300

    add new project from spring.start.io

--- hw3-SpringBoot/src/main/java/ru/otus/spring02/service/KeyboardReaderService.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.spring02.service;
-
-import java.io.IOException;
-
-public interface KeyboardReaderService {
-    String readString() throws IOException;
-    int readInt() throws IOException;
-}
